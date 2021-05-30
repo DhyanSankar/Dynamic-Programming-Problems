4e84e30c19aecf54647c81f87407401772394439
Dhyan Sankar <>
2021-05-30 15:17:30 +0000

    Dhoom 4 partially completed

---
+++ Graphs/Problem 7: Dhoom 4.cpp
@@ -0,0 +1,22 @@
+// Sample code to perform I/O:
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long s, l, N;
+	cin >> s >> ' ' >> l;
+	cin >> N;
+	vector<long long> arr(n), vis(100010, -1);
+	for(int i = 0; i < N; i++){
+		cin >> arr[i];
+	}
+	queue<int> que;
+	que.push(s);
+	visited[s] = 0;
+	while(qu.empty() = false){
+		//code
+		for(long long i:arr){
+			//code
+		}
+	}
+}
